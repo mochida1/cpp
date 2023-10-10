7251d231611d1b4863bd6c05543d68731c4d8749
Henrique Mochida <mochida@Asus.Asus>
2023-10-09 21:29:10 -0300

    subject ok

--- m08/ex02/headers/MutantStack.tpp
+++ m08/ex02/headers/MutantStack.tpp
@@ -6,7 +6,7 @@
 /*   By: mochida <mochida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 12:14:08 by mochida           #+#    #+#             */
-/*   Updated: 2023/08/13 12:35:57 by mochida          ###   ########.fr       */
+/*   Updated: 2023/10/09 20:49:09 by mochida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,32 @@
 #define MUTANTSTACK_TPP
 
 #include <iostream>
-#include <deque>
 #include <stack>
 
-template <class T, class Container = std::deque<T> >
+template <class T, class Container = std::deque<T> > // https://stackoverflow.com/questions/525365/does-stdstack-expose-iterators why we use deque instead of stack
 class MutantStack : public std::stack<T, Container>
 {
 	public:
+	MutantStack(void) {return;};
+	~MutantStack(void) {return;};
+	MutantStack &operator=(MutantStack const &instance) {
+		std::stack<T, Container>::operator=(instance);
+		return *this;
+	};
+	typedef typename Container::iterator iterator;
+	typedef typename Container::reverse_iterator reverse_iterator;
+	iterator begin() {
+		return this->c.begin();
+	};
+	iterator end() {
+		return this->c.end();
+	};
+	reverse_iterator rbegin(void) {
+		return this->c.rbegin();
+	};
+	reverse_iterator rend(void) {
+		return this->c.rend();
+	};
 	private:
 };
 
--- m08/ex02/src/main.cpp
+++ m08/ex02/src/main.cpp
@@ -6,14 +6,72 @@
 /*   By: mochida <mochida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 12:13:36 by mochida           #+#    #+#             */
-/*   Updated: 2023/08/13 12:16:03 by mochida          ###   ########.fr       */
+/*   Updated: 2023/10/09 21:17:41 by mochida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.tpp"
+#include <list>
+
+void TEST_subject_01()
+{
+	std::cout << "<<<<<<<<< starting " <<__FUNCTION__ << "(" << __FILE__ << ":" << __LINE__ << ")" << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	std::cout << "ending " <<__FUNCTION__ << " >>>>>>>>>" << std::endl;
+	return ;
+}
+
+void TEST_subject_02()
+{
+	std::cout << "<<<<<<<<< starting " <<__FUNCTION__ << "(" << __FILE__ << ":" << __LINE__ << ")" << std::endl;
+	std::list<int> mstack;
+	mstack.push_back(5);
+	mstack.push_back(17);
+	std::cout << mstack.front() << std::endl;
+	mstack.pop_back();
+	std::cout << mstack.size() << std::endl;
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	//[...]
+	mstack.push_back(0);
+	std::list<int>::iterator it = mstack.begin();
+	std::list<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	// std::stack<int> s(mstack); we can't use this because there are no constructos in std::stack that use a list object as a parameter. Yes, the subject keeps on fucking up.
+	std::cout << "ending " <<__FUNCTION__ << " >>>>>>>>>" << std::endl;
+	return ;
+}
 
 int main (void)
 {
-	
+	TEST_subject_01();
+	TEST_subject_02();
 	return 0;
 }
