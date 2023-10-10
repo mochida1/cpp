606e49b4190905042549b4d1bd932680818e2794
Henrique Mochida <mochida@Asus.Asus>
2023-10-09 21:45:44 -0300

    feat(m08:ex02): added tests to check wether the iterator effectivelly works

--- m08/ex02/src/main.cpp
+++ m08/ex02/src/main.cpp
@@ -6,12 +6,13 @@
 /*   By: mochida <mochida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 12:13:36 by mochida           #+#    #+#             */
-/*   Updated: 2023/10/09 21:17:41 by mochida          ###   ########.fr       */
+/*   Updated: 2023/10/09 21:44:16 by mochida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.tpp"
 #include <list>
+#include <cassert>
 
 void TEST_subject_01()
 {
@@ -69,9 +70,32 @@ void TEST_subject_02()
 	return ;
 }
 
+void TEST_iterator_validator()
+{
+	std::cout << "<<<<<<<<< starting " <<__FUNCTION__ << "(" << __FILE__ << ":" << __LINE__ << ")" << std::endl;
+	MutantStack<int> mstack;
+	for (int i = 0; i < 66000; i++) // populating all the shit;
+	{
+		mstack.push(i);
+	}
+	MutantStack<int>::iterator it = mstack.begin();
+	for (int i = 0; i < 66000; i++) // here we check wether everything enter in the right order
+	{
+		if (i % 1000 == 0)
+			std::cout << "i[" << i << "]    it[" << *it << "]" << std::endl;
+		assert (i == *it);
+		it++;
+	}
+	std::cout << "ending " <<__FUNCTION__ << " >>>>>>>>>" << std::endl;
+	return ;
+}
+
 int main (void)
 {
 	TEST_subject_01();
 	TEST_subject_02();
+	TEST_iterator_validator();
+	// not implementing tests to check iterators' inherited methods because too lazy to test something that already works.
+	// GL trying to test it if you want.
 	return 0;
 }
